ae49d528608b1bdbc999b980a07028c6ae272669
mrjvs <jellevs@gmail.com>
2021-05-25 15:16:31 +0200

    quick client

--- vivid-server/src/main.ts
+++ vivid-server/src/main.ts
@@ -4,7 +4,9 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
