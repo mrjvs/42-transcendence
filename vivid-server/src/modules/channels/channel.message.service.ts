99efaf19a32ed66a575f62dccfd26d351f26cfd8
pmerku <pmerku@gmail.com>
2021-06-07 15:37:20 +0200

    linting

--- vivid-server/src/modules/channels/channel.message.service.ts
+++ vivid-server/src/modules/channels/channel.message.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Between, DeleteResult, LessThan, MoreThan, Repository } from 'typeorm';
+import { Between, DeleteResult, Repository } from 'typeorm';
 import { Observable, from } from 'rxjs';
 
 import {
