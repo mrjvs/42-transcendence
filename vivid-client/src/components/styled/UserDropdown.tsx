0636f7c203eb07e958fcafedb8b8063c3454f1a1
pmerku <pmerku@gmail.com>
2021-07-12 18:23:01 +0200

    Logout functionality in dropdown menu

--- vivid-client/src/components/styled/UserDropdown.tsx
+++ vivid-client/src/components/styled/UserDropdown.tsx
@@ -4,11 +4,21 @@ import { UserContext } from '../../hooks/useUser';
 import { Avatar } from './Avatar';
 import { Icon } from './Icon';
 import './UserDropdown.css';
+import { useFetch } from '../../hooks/useFetch';
 
 // TODO logout
 export function UserDropdown() {
   const [open, setOpen] = React.useState(false);
   const userData = React.useContext(UserContext);
+
+  const logout = useFetch({
+    url: '/api/v1/auth/logout',
+    method: 'POST',
+  });
+
+  React.useEffect(() => {
+    if (logout.done) window.location.href = '/'; // TODO redirect to homepage
+  }, [logout.done]);
 
   return (
     <div className={'userDropdown ' + (open ? 'open' : '')}>
@@ -24,7 +34,9 @@ export function UserDropdown() {
         <Link to="/settings" className="menuItem">
           User settings
         </Link>
-        <a className="menuItem">Logout</a>
+        <p onClick={() => logout.run()} className="menuItem red">
+          Logout
+        </p>
       </div>
     </div>
   );
